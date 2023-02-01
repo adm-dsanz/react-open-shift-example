fb21e0a993e90d7ec2a346e2c94f07c1e5fd7b6f
adm-dsanz <52535743+adm-dsanz@users.noreply.github.com>
2023-02-01 14:41:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> prueba actualiza.
+          Edit <code>src/App.js</code> prueba actualiza 2.
         </p>
         <a
           className="App-link"
