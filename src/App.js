f8f5f14a7d9d632494774bae81060a0e50df7f47
adm-dsanz <david.sanz@lafargeholcim.com>
2023-02-01 13:56:44 +0100

    update 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save para recargar.
+          Edit <code>src/App.js</code> and guarda para recargar.
         </p>
         <a
           className="App-link"
